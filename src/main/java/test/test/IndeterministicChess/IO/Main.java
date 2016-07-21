4d2691be6041b9ffdba7b13103806c135cbc506f
JulHeg <julheg@gmx.de>
2016-07-21 11:46:19 +0200

    Added MatchController class that now controlls the games

--- src/main/java/test/test/IndeterministicChess/IO/Main.java
+++ src/main/java/test/test/IndeterministicChess/IO/Main.java
@@ -1,8 +1,7 @@
 package test.test.IndeterministicChess.IO;
 
-import javax.swing.SwingUtilities;
-
 import test.test.IndeterministicChess.Board.*;
+import test.test.IndeterministicChess.Match.MatchController;
 import test.test.IndeterministicChess.Piece.*;
 
 
@@ -16,37 +15,9 @@ public class Main{
 		// Schedule a job for the event dispatch thread:
 		// creating and showing this application's GUI.
 		Chessboard chessboard = Chessboard.getStandardChessboard();
-		generalIO blackWindow = new LocalGUIWindow(PieceColor.BLACK, chessboard);
-		generalIO whiteWindow = new LocalGUIWindow(PieceColor.WHITE, chessboard);
-		SwingUtilities.invokeLater(new Thread() {
-			public void run() {
-				blackWindow.showGUI();
-				whiteWindow.showGUI();
-			}
-		});
-		PieceColor activePlayer = PieceColor.WHITE;
-		boolean isLost = false;
-		boolean isDraw = false;
-		while(!isLost && !isDraw){
-			switch(activePlayer){
-			case WHITE:
-				whiteWindow.getResponse();
-				break;
-			case BLACK:
-				blackWindow.getResponse();
-				break;
-			}
-			activePlayer = activePlayer.otherColor();
-			isLost = chessboard.probabilisticHasLost(activePlayer);
-			isDraw = chessboard.probabilisticHasLost(activePlayer);
-		}
-		if(activePlayer == PieceColor.BLACK){
-			blackWindow.showLose();
-			whiteWindow.showWin();
-		}
-		else{
-			blackWindow.showWin();
-			whiteWindow.showLose();
-		}
+		generalIO whiteIO = new LocalGUIWindow(PieceColor.WHITE, chessboard);
+		generalIO blackIO = new LocalGUIWindow(PieceColor.BLACK, chessboard);
+		MatchController match = new MatchController(blackIO, whiteIO, chessboard);
+		match.start();
 	}
 }
--- src/main/java/test/test/IndeterministicChess/Match/MatchController.java
+++ src/main/java/test/test/IndeterministicChess/Match/MatchController.java
@@ -1,7 +1,53 @@
 package test.test.IndeterministicChess.Match;
 
-import test.test.IndeterministicChess.Board.Chessboard;
+import javax.swing.SwingUtilities;
 
-public class MatchController {
-	Chessboard chessboard = Chessboard.getStandardChessboard();
+import test.test.IndeterministicChess.Board.Chessboard;
+import test.test.IndeterministicChess.IO.generalIO;
+import test.test.IndeterministicChess.Piece.PieceColor;
+
+public class MatchController extends Thread{
+	private final Chessboard chessboard;
+	private final generalIO blackIO;
+	private final generalIO whiteIO;
+	
+	public MatchController(generalIO blackIO, generalIO whiteIO, Chessboard chessboard){
+		this.blackIO = blackIO;
+		this.whiteIO = whiteIO;
+		this.chessboard = chessboard;
+	}
+	
+	@Override	
+	public void run(){
+		SwingUtilities.invokeLater(new Thread() {
+			public void run() {
+				blackIO.showGUI();
+				whiteIO.showGUI();
+			}
+		});
+		PieceColor activePlayer = PieceColor.WHITE;
+		boolean isLost = false;
+		boolean isDraw = false;
+		while(!isLost && !isDraw){
+			switch(activePlayer){
+			case WHITE:
+				blackIO.getResponse();
+				break;
+			case BLACK:
+				whiteIO.getResponse();
+				break;
+			}
+			activePlayer = activePlayer.otherColor();
+			isLost = chessboard.probabilisticHasLost(activePlayer);
+			isDraw = chessboard.probabilisticHasLost(activePlayer);
+		}
+		if(activePlayer == PieceColor.BLACK){
+			blackIO.showLose();
+			whiteIO.showWin();
+		}
+		else{
+			blackIO.showWin();
+			whiteIO.showLose();
+		}
+	}
 }
